0ca47dc3015fa3a13dc5026c29797e3bcb7f3040
Ryan Mottley <ligerzero459@gmail.com>
2015-12-21 22:41:05 -0600

    Migration to fix inputs table and update transactions

--- src/migrations/001_add_next_block_hash.rb
+++ src/migrations/001_add_next_block_hash.rb
@@ -2,18 +2,21 @@ require './src/models/block'
 
 Sequel.migration do
   up do
+    puts '001_add_next_block_hash.rb UP'
     alter_table(:blocks) do
       add_column :nextBlockHash, String
     end
 
     blocks = Block.all
     blocks.each do |block|
+      puts block.height.to_s << "/" << blocks.count.to_s << " blocks processed"
       update = DB["UPDATE blocks SET nextBlockHash = ? WHERE blockHash = ?;", block.blockHash, block.previousBlockHash]
       update.update
     end
   end
 
   down do
+    puts '001_add_next_block_hash.rb DOWN'
     alter_table(:blocks) do
       drop_column :nextBlockHash
     end
