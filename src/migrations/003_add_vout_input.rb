ad56e75f7aeb110b2ab730bf91410bb02970240a
Ryan Mottley <ligerzero459@gmail.com>
2015-12-21 22:41:05 -0600

    input fix migration (in progress)

---
+++ src/migrations/003_add_vout_input.rb
@@ -0,0 +1,38 @@
+require 'json'
+
+require './src/models/input'
+require './src/models/transaction'
+require './src/models/raw_transaction'
+
+Sequel.migration do
+  up do
+    alter_table(:inputs) do
+      add_column :vout, Fixnum
+    end
+
+    Input.set_dataset(:inputs)
+
+    raw_transactions = RawTransaction.all
+    raw_transactions.each do |raw_tx|
+      json_tx = JSON.parse(raw_tx.raw)
+      vins = json_tx.fetch("result").fetch("vin")
+      vins.each do |vin|
+        if vin['txid'] != nil
+          old_input = Input.where(:vout=>nil, :outputTxid=> vin['txid']).first
+          old_input.vout = vin['vout'].to_i
+
+          output = Output[]
+
+          old_input.save
+        end
+      end
+    end
+  end
+
+  down do
+    alter_table(:inputs) do
+      drop_column :vout
+    end
+    puts "Unable to undo data changes from migration 3. Fees and input values will still reflect the corrected values"
+  end
+end
