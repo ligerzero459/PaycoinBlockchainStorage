d22ac40d129c88bac4058629e5a08449d1a8fb1e
Ryan Mottley <ligerzero459@gmail.com>
2015-12-25 14:00:21 -0600

    Add coinbase and coinstake identifiers to transaction

---
+++ src/migrations/004_add_coinbase.rb
@@ -0,0 +1,22 @@
+Sequel.migration do
+  up do
+    puts '004_add_coinbase.rb UP'
+    alter_table(:transactions) do
+      add_column :coinbase, TrueClass, :default => false
+      add_column :coinstake, TrueClass, :default => false
+    end
+
+    require './src/models/transaction'
+
+    Transaction.where(:type => 'PoS-Reward', :totalOutput => 0.0, :fees => 0.0).update(:coinbase => true);
+    Transaction.where(:type => 'PoS-Reward').where('totalOutput > 0.0').update(:coinstake => true);
+  end
+
+  down do
+    puts '004_add_coinbase.rb DOWN'
+    alter_table(:transactions) do
+      drop_column :coinbase
+      drop_column :coinstake
+    end
+  end
+end
