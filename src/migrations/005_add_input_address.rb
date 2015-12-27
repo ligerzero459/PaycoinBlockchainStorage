b49a8aabcbedaf8449c67c6673cf88e256196881
Ryan Mottley <ligerzero459@gmail.com>
2015-12-27 10:44:24 -0600

    migration to add address to inputs

---
+++ src/migrations/005_add_input_address.rb
@@ -0,0 +1,28 @@
+Sequel.migration do
+  up do
+    puts '005_add_input_address UP'
+    alter_table (:inputs) do
+      add_column :address, String
+    end
+
+    require './src/models/input'
+    require './src/models/output'
+
+    inputs = Input.all
+    inputs.each do |input|
+      output = Output[:transaction_id => input.outputTransactionId, :n => input.vout]
+
+      if output != nil
+        input.address = output.address
+        input.save
+      end
+    end
+  end
+
+  down do
+    puts '005_add_input_address DOWN'
+    alter_table (:inputs) do
+      drop_column :address
+    end
+  end
+end
